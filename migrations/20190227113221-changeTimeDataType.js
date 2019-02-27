351987a8a2cf3f1a7c523ed480e05e1ff2558d2e
TheOTG <kennythesman@gmail.com>
2019-02-28 01:24:05 +0700

    initial commit

---
+++ migrations/20190227113221-changeTimeDataType.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   return queryInterface.changeColumn('Screenings', 'time', {
+     type: Sequelize.STRING
+   })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+   return queryInterface.changeColumn('Screenings', 'time', {
+     type:Sequelize.DATE
+   })
+  }
+};
