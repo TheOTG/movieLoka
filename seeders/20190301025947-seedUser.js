4a55988c51c95901a10557b179fb59dfc18b6a3f
TheOTG <kennythesman@gmail.com>
2019-03-01 10:01:32 +0700

    seed

---
+++ seeders/20190301025947-seedUser.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   queryInterface.bulkInsert('Users', [{
+     username: 'kenthe',
+     password: '123456',
+     isAdmin: true,
+     email: 'kenthe@mail.com'
+   }])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('Users', null)
+  }
+};
